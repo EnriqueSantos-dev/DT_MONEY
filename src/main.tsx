ea10120901f104909638aedaaf58e2a5e5c4a62a
enrique santos <contatoenriquesantos@gmail.com>
2022-08-21 19:36:27 -0300

    refactor: include wrapper context outside app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { InputsOthersProvider } from "./contexts/Inputs/inputs";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <InputsOthersProvider>
+      <App />
+    </InputsOthersProvider>
   </React.StrictMode>
 );

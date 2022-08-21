0a03370a7b83dff29fa140d8a24d4535c795fec9
enrique santos <contatoenriquesantos@gmail.com>
2022-08-21 19:38:20 -0300

    feat: create functions to manipulation state items

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import { useState } from "react";
 import { Header } from "./components/Header";
 import { Modal } from "./components/Modal";
 import { Panel } from "./components/Panel";
-import { InputsOthersProvider } from "./contexts/Inputs/inputs";
+
+import style from "./app.module.scss";
+import { SearchTransaction } from "./components/SearchTransaction";
+import { TableItems } from "./components/TableItems";
+import { useValuesContext } from "./contexts/Inputs/useInputs";
 
 function App() {
+  const { items, filteredItems } = useValuesContext();
+
   const [isOpen, setIsOpenModal] = useState(false);
 
   return (
     <div className="App">
-      <InputsOthersProvider>
-        <Header setOpenModal={setIsOpenModal} />
+      <Header setOpenModal={setIsOpenModal} />
+      <main className={style.main}>
         <Panel />
-        <Modal isOpen={isOpen} setCloseModal={setIsOpenModal} overlay />
-      </InputsOthersProvider>
+        <div className={style.containerContent}>
+          <SearchTransaction lengthItems={items.length} />
+          <TableItems
+            items={filteredItems.length > 0 ? filteredItems : items}
+          />
+        </div>
+      </main>
+      <Modal isOpen={isOpen} setCloseModal={setIsOpenModal} overlay />
     </div>
   );
 }
